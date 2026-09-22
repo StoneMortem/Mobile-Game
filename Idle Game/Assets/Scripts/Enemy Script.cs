e09c5eed09ab1431caad4baf0f1d24c62a7e59d2
StoneMortem <dluginbu@students.kennesaw.edu>
2026-09-22 14:05:35 -0400

    VERY BASICS!
    Player and Enemy auto attack each other

--- Idle Game/Assets/Scripts/Enemy Script.cs
+++ Idle Game/Assets/Scripts/Enemy Script.cs
@@ -1,16 +1,94 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using System.Collections;
 public class EnemyScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float atkInterval;
+    public float health;
+    public int level;
+    public float dmg;
+    public bool canAtk = true;
+
+    GameObject Player;
+    PlayerScript PS;
+    public SpriteRenderer sr;
+    
+
+    //later, we will probably have an ENUM dictating which enemy type spawns (and any particular stats unique to them)
+    //but for now, I just want to create a generic enemy
+
+
+
+
+    private void Awake()
     {
+        //Placeholder stat assignment
+        atkInterval = 1.5f;
+        health = 10;
+        level = 1;
+        dmg = level * 2;
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PS = Player.GetComponent<PlayerScript>();
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+
+    void Update()
+    {
+        if (canAtk)
+        {
+            Debug.Log("Attacking Player");
+            Atk();
+            
+        }
+        
+    }
+    private void Atk()
+    {
+        PS.TakeDamage(dmg);
+        StartCoroutine(AtkCooldown());
+    }
+    public void Dmg(float damage)
+    {
+        Debug.Log("Enemy took " + damage + ". Remaining health: " + health);
+        StartCoroutine(ColorShift());
+        health -= damage;
+
+       
+        if (health <= 0)
+        {
+            canAtk = false;
+            Die();
+
+        }
+        else {
+            
+        }
+
+    }
+    public IEnumerator ColorShift()
+    {//turns red on taking damage
+     //turns back to white after .2 seconds if it doesn't die.
+        sr.color = Color.red;
+        yield return new WaitForSeconds(.2f);
+        sr.color = Color.white;
+    }
+
+
+    private void Die()
+    {
+        PS.gold += level * 2;
+        Debug.Log("BLEHH!!! Player gained " + level*2 + "gold!");
+        Destroy(gameObject);
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator AtkCooldown()
     {
-        
+        canAtk = false;
+        yield return new WaitForSeconds(atkInterval);
+        canAtk = true;
     }
 }
---
+++ Idle Game/Assets/Scripts/Player Script.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using System.Collections;
+using System;
+public class PlayerScript : MonoBehaviour
+{
+    private float HP = 100;
+    private float dmg = 2;
+    public float gold = 0;
+    private SpriteRenderer Spr;
+    GameObject currentEnemy = null;
+    EnemyScript ES;
+
+
+
+    public bool TEMPATK = true;
+
+    void Start()
+    {
+        Spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentEnemy == null)
+        {
+            try
+            {
+                currentEnemy = GameObject.FindWithTag("Enemy");
+                ES = currentEnemy.GetComponent<EnemyScript>();
+            }
+            catch 
+            { //Debug.Log("No Enemy to target");
+            }
+            
+        }
+        if (TEMPATK && currentEnemy != null) { StartCoroutine(TEMPAUTOATTACK()); }
+        
+    }
+
+    //PLACEHOLDER METHOD
+    public IEnumerator TEMPAUTOATTACK()
+    {
+        
+        TEMPATK = false;
+        ES.Dmg(dmg);
+        StartCoroutine(Attacking());
+        yield return new WaitForSeconds(.5f);
+        TEMPATK = true;
+    }
+
+    public IEnumerator Attacking()
+    {
+        Debug.Log("Attacking " + currentEnemy.name + " for " + dmg + " damage.");
+        Spr.color = Color.yellow;
+        yield return new WaitForSeconds(.1f);
+        Spr.color = Color.white;
+    }
+
+    public IEnumerator TakeDamage(float damage)
+    {
+        Debug.Log("Ow! " + currentEnemy.name + " dealt " + damage + "!");
+        HP -= damage;
+        Spr.color = Color.red;
+        if (HP > 0)
+        {
+            GameOver();
+        }
+        else
+        {
+            yield return new WaitForSeconds(.5f);
+            Spr.color = Color.white;
+        }
+    }
+    
+
+        
+
+    public void GameOver()
+    {
+        Destroy(gameObject);
+        //and probably add ui 
+    }
+
+    public void SpawnNewEnemy()
+    {
+        //placeholder code, this may be put in another script
+    }
+}
